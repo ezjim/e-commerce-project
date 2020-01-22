83c2f0ce753b667918bcfcac371cee9288f01d9d
James <james.re@yahoo.com>
2020-01-22 11:57:02 -0800

    finsihed book.js

--- Books/data/books.js
+++ Books/data/books.js
@@ -1,11 +1,112 @@
-
 const paradiseLost = {
     name: 'Paradise Lost',
     author: 'Robert Milton',
     yearPublished: '1667',
     category: 'novel',
     discription: 'Milton story has two narrative arcs, one about Satan and the other following Adam and Eve. It begins after Satan and the other rebel angels have been defeated and banished to Hell',
-    image: './assets/paradiseLost.jpg',
+    image: './assets/paradiseLost-mil.jpg',
     price: 20.00,
-    cost: 13.00
+
 };
+
+const roadnotTaken = {
+    name: 'Road Not Taken',
+    author: 'Robert Frost',
+    yearPublished: '1993',
+    category: 'novel',
+    discription: 'The Road Not Taken, the meaning of the poem is about a person having to choose between two roads. Two roads diverged in a yellow wood, and the speaker is torn between which road is the best choice.',
+    image: './assets/roadnotTaken-fro.jpg',
+    price: 15.00,
+
+};
+
+const aplaceonEarth = {
+    name: 'A Place on Earth',
+    author: 'Wendell Berry',
+    yearPublished: '1966',
+    category: 'novel',
+    discription: 'In this novel he reveals the deep marriage between the land and its people as a key figure in the story, the farmer Mat Feltner, puts it, "The earth is the genius of our life.',
+    image: './assets/aplaceonEarth-ber.jpg',
+    price: 19.00,
+
+};
+
+const thegiftofGravity = {
+    name: 'The Gift of Gravity',
+    author: 'Wendell Berry',
+    yearPublished: '2003',
+    category: 'Collection of Poems',
+    discription: 'Wendell Berrys poems combine the clarity of masterful craftsmanship with the richness and subtlety of meditative vision.',
+    image: './assets/giftofGravity-ber.jpg',
+    price: 22.00,
+
+};
+
+const thebellJar = {
+    name: 'The Bell Jar',
+    author: 'Sylvia Plath',
+    yearPublished: '1963',
+    category: 'Collection of Poems',
+    discription: 'In The Bell Jar, the main character uses the bell jar as the primary metaphor for feelings of confinement and entrapment. She feels that she is stuck in her own head, spinning around the same thoughts of self-doubt and dejection, over and over again, with no hope of escape.',
+    image: './assets/thebellJar-pla.jpg',
+    price: 26.00,
+
+};
+
+const theColossus = {
+    name: 'The Colossus',
+    author: 'Sylvia Plath',
+    yearPublished: '1960',
+    category: 'Collection of Poems',
+    discription: 'Plath uses the colossus to represent her attempt to reconstruct the father whose absence looms so large in her psyche. By connecting her father to one of the worldS great wonders, she acknowledges his power, and yet she is unable to make him speak, therefore simultaneously stressing his impotence.',
+    image: './assets/theColossus-pla.jpg',
+    price: 26.00,
+
+};
+
+const theUnabridged = {
+    name: 'The Unabridged Journals',
+    author: 'Sylvia Plath',
+    yearPublished: '1982',
+    category: 'Collection of Poems',
+    discription: 'A major literary event -the complete, uncensored journals of Sylvia Plath, published in their entirety for the first time.',
+    image: './assets/theunabridgedjournals-pla.jpg',
+    price: 22.00,
+
+};
+
+const lastnightonEarth = {
+    name: 'The Last Night of the Earth',
+    author: 'Charles Bukowski',
+    yearPublished: '2002',
+    category: 'Collection of Poems',
+    discription: 'Poems deal with writing, death and immortality, literature, city life, illness, war, and the past.',
+    image: './assets/lastnightonearthPoems-buk.jpg',
+    price: 20.00,
+
+};
+
+const talesofordinaryMadness = {
+    name: 'Tales of Ordinary Madness',
+    author: 'Charles Bukowski',
+    yearPublished: '1983',
+    category: 'Collection of Poems',
+    discription: 'This collection of short stories gives an insight into the dark, dangerous lowlife of Los Angeles that Bukowski inhabited. From prostitutes to classical music, Bukowski ingeniously mixes.',
+    image: './assets/talesofordinayMadness-buk.jpg',
+    price: 33.00,
+
+};
+
+const books = [
+    paradiseLost,
+    roadnotTaken,
+    aplaceonEarth,
+    thegiftofGravity,
+    thebellJar,
+    theColossus,
+    theUnabridged,
+    lastnightonEarth,
+    talesofordinaryMadness
+]
+
+export default books;
