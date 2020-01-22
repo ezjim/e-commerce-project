db1231bc8868119fc609f64494c65c6d342ca734
James <james.re@yahoo.com>
2020-01-22 10:58:09 -0800

    rough draft to CSS/html/data

---
+++ Books/data/books.js
@@ -0,0 +1,11 @@
+
+const paradiseLost = {
+    name: 'Paradise Lost',
+    author: 'Robert Milton',
+    yearPublished: '1667',
+    category: 'novel',
+    discription: 'Milton story has two narrative arcs, one about Satan and the other following Adam and Eve. It begins after Satan and the other rebel angels have been defeated and banished to Hell',
+    image: './assets/paradiseLost.jpg',
+    price: 20.00,
+    cost: 13.00
+};
