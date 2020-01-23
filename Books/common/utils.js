e22451a8dbe5b528896c0e052fb3904bd10e5b58
James <james.re@yahoo.com>
2020-01-23 11:39:02 -0800

    test 2 passed

--- Books/common/utils.js
+++ Books/common/utils.js
@@ -7,13 +7,43 @@ const findById = (books, id) => {
     }
 }
 
-export const makePrettyCurrency = (number) =>
+
+const makePrettyCurrency = (number) =>
     number
         .toLocaleString(
-            'en-US',
-            {
+            'en-US', {
                 style: 'currency',
                 currency: 'USD',
             });
 
+
+
+function calcLineTotal(quantity, price) {
+    const amount = quantity * price;
+    console.log(amount);
+    return roundCurrency(amount);
+}
+
+function roundCurrency(amount) {
+    return Math.round(amount * 100) / 100;
+}
+
+function calcOrderTotal(cart, books) {
+    let orderTotal = 0;
+
+    for (let i = 0; i < cart.length; i++) {
+        const lineItem = cart[i];
+        const book = findById(books, lineItem.id);
+        const lineTotal = calcLineTotal(lineItem.quantity, book.price);
+        orderTotal += lineTotal;
+    }
+    return roundCurrency(orderTotal);
+}
+
 export default findById;
+export {
+    calcLineTotal,
+    roundCurrency,
+    makePrettyCurrency,
+    calcOrderTotal
+};
--- test/example.test.js
+++ test/example.test.js
@@ -1,7 +1,11 @@
 // IMPORT MODULES under test here:
 // import findById from '../Books/common/utils.js';
-// import books from '../Books/data/cart.js';
-import renderBooks from '../Books/data/render-book.js';// import example from '../src/example.js';
+import books from '../Books/data/cart.js';
+import renderBooks from '../Books/data/render-book.js'; // import example
+import { calcLineTotal } from '../Books/common/utils.js';
+import cart from '../Books/common/utils';
+import { calcOrderTotal } from '../Books/common/utils.js';
+// import roundCurrecny from '../Books/common/utils.js';
 
 const test = QUnit.test;
 
@@ -14,9 +18,9 @@ test('findById', function(assert) {
         category: 'novel',
         discription: 'Milton story has two narrative arcs, one about Satan and the other following Adam and Eve. It begins after Satan and the other rebel angels have been defeated and banished to Hell',
         image: './assets/paradiseLost-mil.jpg',
-        price:  20.00,  
+        price: 20.00,
     };
-    
+
     // Set up your parameters and expectations
     const expected = '<li class=\"novel\" title=\"Paradise Lost\"><h3></h3><img src=\"./assets/./assets/paradiseLost-mil.jpg\" alt=\"undefinedimage\"><p class=\"price\">$20.00<button value=\"Paradise Lost\">Add</button></p></li>';
     const result = renderBooks(paradiseLost).outerHTML;
@@ -26,4 +30,33 @@ test('findById', function(assert) {
     //Assert
     // Make assertions about what is expected valid result
     assert.equal(expected, result);
+});
+
+test('calculate line total', (assert) => {
+
+    // arrange
+
+    const quantity = 2;
+
+    const price = 20.00;
+
+    const expected = 40.00;
+
+    // act 
+
+    const total = calcLineTotal(quantity, price);
+
+    // assert
+
+    assert.equal(total, expected);
+
+});
+
+test('calculate order total', (assert) => {
+
+    const expected = 60.00;
+
+    const orderTotal = calcOrderTotal(cart, books);
+
+    assert.equal(orderTotal, expected);
 });
